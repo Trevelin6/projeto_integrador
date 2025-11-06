2486d55ae93dceebcd31950b6c3dbd5808c5da6c
EA - Felipe Trevelin De Godoi <felipe_trevelin@educadventista.org>
2025-11-05 22:07:48 -0300

    inserção de comentarios

---
+++ projeto_integrador-main/Cliente.java
@@ -0,0 +1,47 @@
+import java.io.*;
+//input/output
+import java.net.*;
+//rede
+import java.util.Scanner;
+//leitura do teclado
+
+public class Cliente {
+    public static void main(String[] args) {
+        String servidor = "localhost";
+        //endereço do servidor
+        int porta = 12345;
+        //porta do servidor
+
+        try (Socket socket = new Socket(servidor, porta)) {
+            //tenta conexão com servidor, cria socket TCP 
+            System.out.println("Conectado ao servidor.");
+
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //objeto para input
+            PrintWriter saida = new PrintWriter(socket.getOutputStream(), true);
+            //objeto para output
+
+            Scanner sc = new Scanner(System.in);
+            //objeto leitura do teclado
+
+            System.out.print("Digite os números separados por espaço: ");
+            String numeros = sc.nextLine();
+
+            saida.println(numeros);
+
+            String resposta = entrada.readLine();
+            //recebe resposta do servidor
+            System.out.println("Números ordenados recebidos do servidor: " + resposta);
+
+            socket.close();
+            //fecha socket TCP de conexão com servidor
+            
+            sc.close();
+            //encerra o Scanner
+            System.out.println("Conexão encerrada.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ projeto_integrador-main/Servidor.java
@@ -0,0 +1,62 @@
+import java.io.*;
+//gerenciamento de input/output
+import java.net.*;
+//rede
+import java.util.*;
+//utilitarios
+
+public class Servidor {
+    public static void main(String[] args) {
+        int porta = 12345;
+        //porta TCP que o server irá escutar
+
+        try (ServerSocket servidor = new ServerSocket(porta)) {
+            //cria um Server-Socket atrelado á porta
+            //por ser aberto como parametro no try-catch, vai ser fechado sozinho
+            System.out.println("Servidor iniciado na porta " + porta);
+            System.out.println("Aguardando conexão...");
+
+            Socket cliente = servidor.accept();
+            //interrompe a execução do código até que um cliente se conecte
+            System.out.println("Cliente conectado: " + cliente.getInetAddress());
+
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+            //objeto para leitura de input
+            PrintWriter saida = new PrintWriter(cliente.getOutputStream(), true);
+            //objeto para envio de output
+
+            String numerosRecebidos = entrada.readLine();
+            System.out.println("Recebendo: " + numerosRecebidos);
+
+            String[] partes = numerosRecebidos.split(" ");
+            //separa o input em um array de String
+            int[] numeros = new int[partes.length];
+            //cria array de numeros de mesmo tamanho
+            for (int i = 0; i < partes.length; i++) {
+                numeros[i] = Integer.parseInt(partes[i]);
+                //converte String para int e registra no array de int
+            }
+
+            Arrays.sort(numeros);
+            //realiza a ordenação
+            System.out.println("Ordenando...");
+
+            StringBuilder resposta = new StringBuilder();
+            //objeto para formatar a repsosta
+            for (int n : numeros) {
+                resposta.append(n).append(" ");
+            }
+
+            saida.println(resposta.toString().trim());
+            //envio de input para o cliente
+            System.out.println("Enviando números ordenados: " + resposta.toString().trim());
+
+            cliente.close();
+            //fecha o socket unico do cliente
+            System.out.println("Conexão encerrada.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
